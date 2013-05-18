590714027ffbec1a2e7122e580f381220b12df6c
Gregory Jefferis <jefferis@gmail.com>
2013-05-18 14:46:28 +0100

    bookmarks/auxfile parsing functions

---
+++ R/pdfbookmarks.r
@@ -0,0 +1,44 @@
+#' @rdname insert_bookmarks
+#' @return location of pdftk bookmark file
+#' export
+extract_bookmarks<-function(pdfin,bookmarks){
+  if(missing(bookmarks)) bookmarks=sub("\\.pdf$",".info",basename(pdfin))
+  cmd=sprintf("%s %s  dump_data > %s",pdftk(),shQuote(pdfin),shQuote(bookmarks))
+  RunCmdForNewerInput(cmd,pdfin,bookmarks)
+  bookmarks
+}
+
+#' Insert and Extract bookmarks from a pdf
+#' @param pdfin,pdfout Input and Output pdfs
+#' @param bookmarks Path to a bookmarks file in pdf format
+#' @return location of modified pdf
+#' @author jefferis
+#' @export
+#' @seealso \code{\link{make_bookmark_text}}
+insert_bookmarks<-function(pdfin,bookmarks,pdfout=pdfin){
+  if(pdfin==pdfout){
+    real_out=pdfout
+    pdfout=tempfile(basename(pdfout),tmpdir=dirname(pdfout),fileext='.pdf')
+    on.exit(file.rename(pdfout,real_out))
+  }
+  cmd=sprintf("%s %s  update_info %s output %s",pdftk(),shQuote(pdfin),
+      shQuote(bookmarks),shQuote(pdfout))
+  RunCmdForNewerInput(cmd,c(pdfin,bookmarks),pdfout)
+  pdfout
+}
+
+#' Make a correctly formatted block of text defining a bookmark in pdftk format
+#' 
+#' @param title The title of the bookmark
+#' @param level The level of the bookmark (1 is the highest)
+#' @param page Absolute page that this refers to
+#' @return String that can be written to bookmarks file
+#' @author jefferis
+#' @export
+#' @seealso \code{\link{extract_bookmarks}}
+#' @examples
+#' make_bookmark_text('Figure 1',3,20)
+make_bookmark_text<-function(title,level,page){
+  sprintf("BookmarkBegin\nBookmarkTitle: %s\nBookmarkLevel: %d\nBookmarkPageNumber: %d",
+      title,level,page)
+}
---
+++ R/pdflatexutils.r
@@ -0,0 +1,40 @@
+# functions to handle parsing of pdflatex files
+
+#' Find location of figures or tables using latex auxfile
+#' 
+#' @param auxfile Path to aux file produced by latex (inc LyX)
+#' @param ftype Whether to look for figures or tables
+#' @return Named integer page number sequence for figures/tables
+#' @author jefferis
+#' @export
+find_figs<-function(auxfile,ftype=c("fig","table")){
+  ftype=match.arg(tolower(ftype))
+  auxentry=ifelse(ftype=="Fig","lof","lot")
+  t=readLines(auxfile)
+  tt=t[grep(paste("{",auxentry,"}",sep=""),t,perl=TRUE)]
+  figurefiles=sub(".*numberline {(\\d+)}.*}{\\d+}{.*",paste(ftype,"\\1.pdf",sep=""),tt,perl=TRUE)
+  figurepages=as.integer(sub(".*numberline {\\d+}.*}{(\\d+)}{.*","\\1",tt,perl=TRUE))
+  names(figurepages)=figurefiles
+  na.omit(figurepages)
+}
+
+#' Find location of supplemental information (SI) using latex auxfile
+#' 
+#' This can be confused by entries in table of contents etc, in which case the
+#' explicit parameter lastRegularPage can be passed.
+#' @param auxfile Path to aux file produced by latex (inc LyX)
+#' @param numpages Number of pages in pdf (assuming this is where SI stops)
+#' @param lastRegularPage Last page before SI
+#' @return Integer page number sequence for SI
+#' @author jefferis
+#' @export
+find_supplemental<-function(auxfile,numpages,lastRegularPage){
+  if(missing(lastRegularPage)){
+    t=readLines(auxfile)
+    tt=t[grep(paste("{Supplementa(l|ry) Information}",sep=""),t,perl=TRUE,ignore.case=TRUE)]
+    supplementalStart=as.integer(sub(".*{section}.*}{(\\d+)}{.*","\\1",tt,perl=TRUE))
+  } else {
+    supplementalStart=lastRegularPage+1
+  }
+  seq(from=supplementalStart,to=numpages)
+}
