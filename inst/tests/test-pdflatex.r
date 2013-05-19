2b2926682965c977a75ba097ed49f7bde6eca9c0
Gregory Jefferis <jefferis@gmail.com>
2013-05-19 20:03:13 +0100

    Add test for locating figures

---
+++ inst/tests/test-pdflatex.r
@@ -0,0 +1,12 @@
+context("Test handling of pdflatex pdfs")
+
+test_that("Find figures pages from pdf", {
+  lyxtmpdir=file.path(dirname(getwd()),'lyx_tmpdir')
+  expect_warning(figs<-find_figs(file.path(lyxtmpdir,'test.aux')),
+                 regexp='NAs introduced',
+                 info='warning due to omitted SI figure')
+                
+  baseline=structure(3:4, .Names = c("Fig1.pdf", "Fig2.pdf"))
+  expect_equivalent(figs, baseline,
+                    'Main figures identified')
+})
