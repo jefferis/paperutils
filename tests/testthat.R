380324c213f519706cdab8536d57a77de88d1972
Gregory Jefferis <jefferis@gmail.com>
2014-12-01 16:04:06 +0000

    Fix persistent parse error re if/else
    * thought this layour worked OK when sourced.

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,3 +1,7 @@
 library(testthat)
-if(.Platform$OS.type=="unix") test_check("paperutils")
-else message("Skipping tests on non-unix platform! Patches for Windows welcome.")
+
+if(.Platform$OS.type=="unix") {
+  test_check("paperutils")
+} else {
+  message("Skipping tests on non-unix platform! Patches for Windows welcome.")
+}
