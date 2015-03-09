c145b1c00ec4a6bd46e8856e0881148e802debca
Gregory Jefferis <jefferis@gmail.com>
2015-03-09 22:43:26 +0000

    fix path in bibtex cleanup test

--- tests/testthat/test-cv.r
+++ tests/testthat/test-cv.r
@@ -2,7 +2,7 @@ context("cv")
 
 if(nzchar(bibtool_path(mustWork=FALSE))){
 test_that("clean bibtex file", {
-  bibin=system.file("tests/testthat/testdata/bib/bibdesk.bib", package='paperutils')
+  bibin="testdata/bib/bibdesk.bib"
   expect_is(bibout<-bibdesk_clean(bibin), 'character')
   # modulo line endings I guess
   expect_equivalent(tools::md5sum(bibout), "5451e1662921cddffd7cbc264ce8f587")
