f0f0c417352cebe7134dd4137a8513d0e5094eba
Gregory Jefferis <jefferis@gmail.com>
2018-01-03 13:48:44 +0000

    Fix expectation for testthat v2
    * same label problem that I had in nat

--- tests/testthat/test-pdflatex.r
+++ tests/testthat/test-pdflatex.r
@@ -1,12 +1,11 @@
 context("Test handling of pdflatex pdfs")
 
 test_that("Find figures pages from pdf", {
-  lyxtmpdir=file.path(getwd(),'testdata','lyx_tmpdir')
+  lyxtmpdir=file.path('testdata','lyx_tmpdir')
   expect_warning(figs<-find_figs(file.path(lyxtmpdir,'test.aux')),
                  regexp='NAs introduced',
                  info='warning due to omitted SI figure')
                 
   baseline=structure(3:4, .Names = c("Fig1.pdf", "Fig2.pdf"))
-  expect_equivalent(figs, baseline,
-                    'Main figures identified')
+  expect_equivalent(figs, baseline)
 })
