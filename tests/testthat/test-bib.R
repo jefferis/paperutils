9a812252ddc0196e87aed596802efcd20a7d2002
Gregory Jefferis <jefferis@gmail.com>
2019-09-18 17:17:40 +0100

    Make bibtool test conditional

--- tests/testthat/test-bib.R
+++ tests/testthat/test-bib.R
@@ -1,6 +1,7 @@
 context("Test handling of bib files")
 
 test_that("Read a BibDesk bib file", {
+  skip_if_not(nzchar(bibtool_path(mustWork = F)))
   f='testdata/bib/bibdesk.bib'
   expect_is(read_bibdesk_bib(f), "BibEntry")
 })
